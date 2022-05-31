bce726dd1518536028e4e70de3770d9468c7561c
jorge <jorgef2465@gmail.com>
2022-05-31 09:10:58 -0500

    add eureka server and eureka clients

--- product/src/main/java/com/example/product/client/customer/CustomerClient.java
+++ product/src/main/java/com/example/product/client/customer/CustomerClient.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "service-customer", url = "http://localhost:8081")
+//@FeignClient(name = "service-customer", url = "http://localhost:8081")
+@FeignClient(name = "service-customer")
 public interface CustomerClient {
     @GetMapping("/api/type/customer/{id}")
     TypeCustomer getTypeCustomerFeign(@PathVariable long id);
--- transaction/src/main/java/com/nttdata/app/transaction/client/account/AccountClient.java
+++ transaction/src/main/java/com/nttdata/app/transaction/client/account/AccountClient.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "service-customer", url = "http://localhost:8083")
+//@FeignClient(name = "service-customer", url = "http://localhost:8083")
+@FeignClient(name = "service-customer")
 public interface AccountClient {
     @GetMapping("/api/account/{id}")
     Account getAccountFeign(@PathVariable long id);
